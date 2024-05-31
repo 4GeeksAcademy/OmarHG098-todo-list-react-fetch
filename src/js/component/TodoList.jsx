6f4e301f61284790d2627b4d16f1e6d158e48ffc
Omar Hidalgo <omar_ehr@outlook.com>
2024-05-31 05:07:56 +0000

    ADD getInfo

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,11 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const TodoList = () => {
-  const [tasksArray, setTasksArray] = useState([]);
+  const urlAPI = "https://playground.4geeks.com/todo";
   const [task, setTask] = useState("");
+  const [tasksArray, setTasksArray] = useState([]);
+  const [userData, setUserData] = useState({
+    name: "",
+    id: "",
+    todos: {
+      label: "",
+      isDone: false,
+    },
+  });
+
+  async function getUserInfo() {
+    try {
+      const response = await fetch(`https://playground.4geeks.com/todo/users/OmarHG098`);
+      const infoUser = response.json();
+      console.log(infoUser);
+      setUserData(infoUser);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  function handleChange (e) {
+    setUserData({... user, [e.target.name]: e.target.value});
+  }
+
+  useEffect(()=> {
+    getUserInfo();
+  }, [])
 
   function addTask(event) {
-    // setTasksArray([...tasks, ]);
     if (event.key === "Enter") {
       setTask(event.target.value);
       setTasksArray([...tasksArray, task]);
@@ -13,21 +40,22 @@ const TodoList = () => {
   }
 
   function deleteTask(id) {
-    // const newArray = tasksArray.filter((task, index) => {
-    //   if (index !== id) {
-    //     return task;
-    //   }
-    // });
     const eraseTask = tasksArray.filter((task, index) => index !== id);
     setTasksArray(eraseTask);
-  
-    
   }
 
   return (
     <div className="container-flex align-items-center">
       <div className="pt-2 fs-1 text-secondary">todos</div>
       <div className="card mt-3 mx-auto p-1" style={{ width: "30rem" }}>
+        <input className="form-control"
+        type="text"
+        placeholder="Username"
+        name="name"
+        value={userData.name}
+        >
+
+        </input>
         <input
           className="form-control"
           type="text"
@@ -39,7 +67,10 @@ const TodoList = () => {
         <ul className="list-group d-flex justify-content-around my-1">
           {tasksArray.map((task, id) => {
             return (
-              <li className="list-group-item d-flex justify-content-between px-3 tarea" key={`task-${id}`}>
+              <li
+                className="list-group-item d-flex justify-content-between px-3 tarea"
+                key={`task-${id}`}
+              >
                 {task}
                 <button
                   type="button"
@@ -52,7 +83,9 @@ const TodoList = () => {
             );
           })}
           <div className="my-1" key={"items-left"}>
-            {tasksArray.length > 0 ? `${tasksArray.length} items left` : "No tasks left"} 
+            {tasksArray.length > 0
+              ? `${tasksArray.length} items left`
+              : "No tasks left"}
           </div>
         </ul>
       </div>
